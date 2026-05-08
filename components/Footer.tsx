deb2969392a4d400143ba239e9677832686a1ba5
daffapratama123-TI <daffap8233@gmail.com>
2026-05-08 22:33:32 +0700

    DAFFA:menambahkan deskripsi wisata bromo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer-container"></div>
       <h3>Tentang Wisata Bromo</h3>
+      <p>Destinasi wisata terbaik di Jawa Timur dengan pemandangan gunung berapi yang eksotis</p>
     </footer>
   )
 }
