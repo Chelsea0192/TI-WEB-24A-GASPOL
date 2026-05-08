5f7695fd42d75a4a77631adcb36c84e503501aa2
daffapratama123-TI <daffap8233@gmail.com>
2026-05-08 22:32:11 +0700

    DAFFA: membuat section tentang wisata bromo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container"></div>
+      <h3>Tentang Wisata Bromo</h3>
     </footer>
   )
 }
