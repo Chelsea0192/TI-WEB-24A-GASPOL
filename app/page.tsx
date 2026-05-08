de4f659cdad3fd054976ebf265926db459e077a4
arga_naufal_anas@teknokrat.ac.id <chelsea_fyantika_agustin@teknokrat.ac.id>
2026-05-08 16:53:09 +0700

    Arga:Menambahkan komponen Overview untuk menampilkan informasi wisata Bromo

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
 import Overview from '@/components/Overview'
+
+export default function Home() {
+    const overviewText = 
+    "Selamat datang di website wisata Gunung Bromo! Gunung Bromo adalah salah satu gunung berapi paling terkenal di Indonesia yang terletak di Jawa Timur. Dengan ketinggian 2.329 meter di atas permukaan laut, Bromo menawarkan pemandangan sunrise yang spektakuler, lautan pasir yang memukau, dan kawah yang masih aktif. Destinasi ini menjadi favorit wisatawan lokal maupun mancanegara yang ingin menikmati keindahan alam yang eksotis."
+  
+    return (
+    <></>
+    )
+}
