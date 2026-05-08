1805274cc506306d289c6cf4a3d0545882cb92f2
arga_naufal_anas@teknokrat.ac.id <chelsea_fyantika_agustin@teknokrat.ac.id>
2026-05-08 17:03:04 +0700

    Arga:Menambahkan hero section halaman home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Overview from '@/components/Overview'
 
 export default function Home() {
-    const overviewText = 
-    "Selamat datang di website wisata Gunung Bromo! Gunung Bromo adalah salah satu gunung berapi paling terkenal di Indonesia yang terletak di Jawa Timur. Dengan ketinggian 2.329 meter di atas permukaan laut, Bromo menawarkan pemandangan sunrise yang spektakuler, lautan pasir yang memukau, dan kawah yang masih aktif. Destinasi ini menjadi favorit wisatawan lokal maupun mancanegara yang ingin menikmati keindahan alam yang eksotis."
-  
-    return (
-    <></>
-    )
+
+  const overviewText =
+    "Selamat datang di website wisata Gunung Bromo!"
+
+  return (
+    <>
+      <div className="hero">
+         <h1>🌄 Pesona Gunung Bromo</h1>
+      </div>
+    </>
+  )
 }
