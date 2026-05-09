87baa359e4c73cd19201e2ec1737a579972de0a1
daffapratama123-TI <daffap8233@gmail.com>
2026-05-09 11:38:44 +0700

    DAFFA: menambahkan deskripsi website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'Wisata Bromo',
+  description: 'Website wisata Gunung Bromo',
+}
