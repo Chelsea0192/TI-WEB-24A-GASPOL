d2eb2a93b969a7bd769c8429288dd32997e940e7
Arganaufal123 <arga_naufal_anas@teknokrat.ac.id>
2026-05-09 15:08:39 +0700

    Arga:membuat section harga tiket

--- app/info-wisata/page.tsx
+++ app/info-wisata/page.tsx
@@ -6,6 +6,9 @@ export default function InfoWisataPage() {
      <p>Informasi lengkap untuk kunjungan Anda</p>
     </div>
 
+      <div className="info-section">
+        
+      </div>
     </>
   )
 }
