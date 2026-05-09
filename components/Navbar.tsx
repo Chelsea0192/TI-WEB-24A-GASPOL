e1960bfef465f05224c2591abbf73d108749a1c9
Chelsea0192 <chelsea_fyantika_agustin@teknokrat.ac.id>
2026-05-09 12:25:11 +0700

    chelsea: menyelesaikan navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,37 @@
 'use client'
+
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
-     const pathname = usePathname()
-     const navItems = [
+  const pathname = usePathname()
+  
+  const navItems = [
     { name: 'Home', path: '/' },
     { name: 'Bromo', path: '/bromo' },
     { name: 'Info Wisata', path: '/info-wisata' },
     { name: 'Hubungi Kami', path: '/hubungi-kami' },
   ]
-
+  
   return (
-  <nav className="navbar">
+    <nav className="navbar">
       <div className="navbar-container">
-      <Link href="/" className="navbar-logo">
-        🌋 Wisata Bromo
-      </Link>
-       <ul className="nav-menu">
-         {navItems.map((item) => (
-          <li key={item.path}>
-            <Link href={item.path}>
+        <Link href="/" className="navbar-logo">
+          🌋 Wisata Bromo
+        </Link>
+        <ul className="nav-menu">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <Link 
                 href={item.path} 
                 className={`nav-link ${pathname === item.path ? 'active' : ''}`}
+              >
                 {item.name}
-            </Link>
-          </li>
-
-        ))}
-
-
-       </ul>
-    </div>
-  </nav>
-)
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
 }
