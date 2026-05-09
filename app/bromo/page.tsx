6867a8d4b51bab8659add138283f27bf41edc78d
Chelsea0192 <chelsea_fyantika_agustin@teknokrat.ac.id>
2026-05-09 12:41:47 +0700

    chelsea: Menambahkan informasi sejarah Gunung Bromo

--- app/bromo/page.tsx
+++ app/bromo/page.tsx
@@ -1,6 +1,12 @@
 import Overview from '@/components/Overview'
 export default function BromoPage() {
-    const overviewText = "Gunung Bromo adalah destinasi wisata unggulan di Jawa Timur."
+ const overviewText = `Gunung Bromo adalah destinasi wisata unggulan di Jawa Timur yang menawarkan
+  keindahan alam luar biasa. Terletak di kawasan Bromo Tengger Semeru National Park,
+  gunung ini memiliki ketinggian 2.329 MDPL.`;  
+  
+  const sejarahText = `Nama Bromo berasal dari kata Brahma, yaitu dewa api dalam
+  kepercayaan Hindu. Masyarakat Tengger yang tinggal di sekitar kawasan ini
+  masih menjaga tradisi dan budaya leluhur.`;
 
-
+  
 }
