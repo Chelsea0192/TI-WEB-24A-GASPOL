9ab10e1dad33ca941c7b4c9147fb2b7ae4bc9f3e
daffapratama123-TI <daffap8233@gmail.com>
2026-06-08 20:00:01 +0700

    daffa :menambahkan biaya kendaraan roda empat

--- app/info-wisata/page.tsx
+++ app/info-wisata/page.tsx
@@ -21,6 +21,7 @@ export default function InfoWisataPage() {
           <li>Wisatawan Domestik: Rp 30.000 - Rp 50.000</li>
           <li>Wisatawan Asing: Rp 150.000 - Rp 300.000</li>
           <li>Kendaraan Roda 2: Rp 10.000</li>
+          <li>Kendaraan Roda 4: Rp 25.000</li>
         </ul>
       </div>
       </div>
