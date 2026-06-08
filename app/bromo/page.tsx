57c84fd74f3db850b5e1e135c26b91629293e407
chelsea0192 <chelsea_fyantika_agustin@teknokrat.ac.id>
2026-06-08 19:13:37 +0700

    Chelsea : menambahkan deskripsi umum Gunung Bromo

--- app/bromo/page.tsx
+++ app/bromo/page.tsx
@@ -11,7 +11,11 @@ import Footer from '../../components/Footer';
       <Navbar />
       <div className="container">
       <div className="bromo-content">
-         <h2>Mengenal Gunung Bromo</h2>
+        <h2>Mengenal Gunung Bromo</h2>
+        <p>Gunung Bromo adalah salah satu gunung berapi paling terkenal di Indonesia, terletak di Jawa Timur.
+         Dengan ketinggian 2.329 meter di atas permukaan laut, Bromo menawarkan pemandangan yang spektakuler, 
+         terutama saat matahari terbit.</p>
+
       </div>
       </div>
       <Footer />
