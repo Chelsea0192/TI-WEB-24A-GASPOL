a079baf35fda1ea91a9d0d50088066314c8919b1
Arganaufal123 <arga_naufal_anas@teknokrat.ac.id>
2026-06-12 21:51:38 +0700

    Arga :Membuat komponen utama halaman Hubungi Kami

--- app/hubungi-kami/page.tsx
+++ app/hubungi-kami/page.tsx
@@ -4,4 +4,7 @@ import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 
+export default function HubungiKamiPage() {}
+
+
 
