f52feb9a8a22befe62ae4404513c0f88a370db86
Arganaufal123 <arga_naufal_anas@teknokrat.ac.id>
2026-06-12 21:56:16 +0700

    Arga :Menambahkan field email pada state form

--- app/hubungi-kami/page.tsx
+++ app/hubungi-kami/page.tsx
@@ -7,7 +7,7 @@ import Footer from '../../components/Footer';
 export default function HubungiKamiPage() {
       const [formData, setFormData] = useState({
         name: '',
-
+        email: '',
 
 });
 }
